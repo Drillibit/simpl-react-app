93026f1b0e401e1bc67eed578f0eae6cf4998cb2
Drillibit <293344asd@gmail.com>
2017-09-11 14:24:56 +0600

    arrays, loops and prop types in react

--- src/app.js
+++ src/app.js
@@ -2,61 +2,84 @@ import React from 'react';
 import ReactDom from 'react-dom';
 require('./style/css/main.sass');
 
+var PLAYERS = [
+  {
+    name: "Horhe Rodriges",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Migel Escobar",
+    score: 34,
+    id: 2,
+  },
+  {
+    name: "Anna Kudos",
+    score: 27,
+    id: 3,
+  },
+  {
+    name: "Bob Marley",
+    score: 43,
+    id: 4,
+  }
+]
+
+
 function Header(props){
-  return(
+  return (
     <div className="header">
       <h1>{props.title}</h1>
     </div>
   );
 }
-
 Header.propTypes = {
   title: React.PropTypes.string.isRequired,
-};
+}
 
-
-
+function Player(props){
+  return(
+    <div className="player">
+      <div className="player-name">{props.name}</div>
+      <div className="player-score">
+        <div className="counter">
+          <div className="counter-action decrement"> - </div>
+          <div className="counter-score"> {props.score} </div>
+          <div className="counter-action increment"> + </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+Player.propTypes ={
+  name: React.PropTypes.string,
+  score: React.PropTypes.number,
+}
 function Application(props) {
   return (
     <div className="scoreboard">
-      <Header title={props.title} />
+      <Header title={props.title}/>
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Jim Hoskins
-          </div>
-          <div className="player-score">
-            <div className="counter">
-            <div className="counter-action decrement"> - </div>
-            <div className="counter-score">31</div>
-            <div className="counter-action increment"> + </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Ann Marsh
-          </div>
-          <div className="player-score">
-            <div className="counter">
-            <div className="counter-action decrement"> - </div>
-            <div className="counter-score">43</div>
-            <div className="counter-action increment"> + </div>
-            </div>
-          </div>
-        </div>
+        {props.players.map(function(player){
+          return <Player name={player.name} score={player.score} key={player.id} />
+        })}
       </div>
     </div>
   );
 }
 
 Application.propTypes = {
-  title: React.PropTypes.string ,
+  title: React.PropTypes.string,
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired,
 };
+
 Application.defaultProps = {
-  title: "Scoreboard",
-}
-ReactDom.render(<Application />, document.querySelector('.container'));
+  title: 'Scoreboard',
+};
+
+
+ReactDom.render(<Application players={PLAYERS} />, document.querySelector('.container'));
