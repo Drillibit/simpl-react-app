a0f33940cbf1ed0db3699c3186de5df65924c61a
Drillibit <293344asd@gmail.com>
2017-09-21 22:56:55 +0600

    app complite

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style/css/main.sass';
+import App from './components/App';
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "build")
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      minify: {
+        collapseWhitespace: true
+      },
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.sass$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader", options: {
+            sourceMap: true
+          }
+        },{
+          loader: "sass-loader", options: {
+            sourceMap: true
+          }
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+      //end
+    ]
+  }
+};
